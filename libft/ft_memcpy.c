9fd1a8cdef9eb3714b66c95451eff55d2b78c4f1
oxo4real <abduredoxo@hotmail.com>
2024-10-23 15:53:20 +0100

    made some improvements

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -17,7 +17,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	unsigned char		*d;
 	const unsigned char	*s;
 
-	if (dest || src || n)
+	if (dest || src)
 	{
 		d = (unsigned char *)dest;
 		s = (const unsigned char *)src;
