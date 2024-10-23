3480f1d23fb145699daff5cb2ec8d45af43844cc
oxo4real <abduredoxo@hotmail.com>
2024-10-23 15:51:23 +0100

    made some improvements

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -37,7 +37,13 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	if (s)
 	{
 		if (start >= ft_strlen(s))
-			return (NULL);
+		{
+			rendu = (char *)malloc(1);
+			if (!rendu)
+				return (NULL);
+			rendu[0] = 0;
+			return (rendu);
+		}
 		if (ft_strlen(s) - start < len)
 			len = ft_strlen(s) - start;
 		rendu = (char *)malloc(len + 1);
